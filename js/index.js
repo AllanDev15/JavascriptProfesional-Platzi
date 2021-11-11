1be58c251ad653a46bde427eba76664de544b093
Allan_PC <Allan.FL.15@live.com>
2021-11-10 20:33:00 -0600

    add autoplay Plugin

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 import MediaPlayer from './mediaPlayer.js';
+import AutoPlay from './plugins/autoPlay.js';
 
 const video = document.querySelector('video');
 const button = document.querySelector('button');
 
-const player = new MediaPlayer({ element: video });
+const player = new MediaPlayer({ element: video, plugins: [new AutoPlay()] });
 button.onclick = () => player.togglePlay();
