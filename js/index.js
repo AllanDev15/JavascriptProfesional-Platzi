1a0e2dfef796c28f9d4c75d5c355f91c4659db38
Allan_PC <Allan.FL.15@live.com>
2021-11-19 13:43:28 -0600

    implements Visibility change listener

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 import MediaPlayer from './mediaPlayer.js';
 import AutoPlay from './plugins/autoPlay.js';
+import AutoPause from './plugins/autoPause.js';
 
 const video = document.querySelector('video');
 const play = document.querySelector('#play');
 const mute = document.querySelector('#mute');
 
-const player = new MediaPlayer({ element: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({ element: video, plugins: [new AutoPlay(), new AutoPause()] });
 play.onclick = () => player.togglePlay();
 mute.onclick = () => player.toggleMute();
