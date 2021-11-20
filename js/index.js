bd681f60d2eb3a5145b6f504b5b6b491e85bce15
Allan_PC <Allan.FL.15@live.com>
2021-11-19 19:51:32 -0600

    validate service worker in the navigator

--- js/index.js
+++ js/index.js
@@ -9,3 +9,9 @@ const mute = document.querySelector('#mute');
 const player = new MediaPlayer({ element: video, plugins: [new AutoPlay(), new AutoPause()] });
 play.onclick = () => player.togglePlay();
 mute.onclick = () => player.toggleMute();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch((error) => {
+    console.log(error.message);
+  });
+}
